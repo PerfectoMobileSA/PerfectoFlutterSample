d1825551f4c53288e3f13630ccb7197670563f67
Nato Vasilevski <38321446+Nato88@users.noreply.github.com>
2022-07-24 18:00:26 +0300

    Adding Folders part1

---
+++ testing_app/android/app/src/androidTest/java/dev/flutter/testing_app/MainActivityTest.java
@@ -0,0 +1,13 @@
+package dev.flutter.testing_app;
+
+import androidx.test.rule.ActivityTestRule;
+//import dev.flutter.plugins.e2e.FlutterTestRunner; // Original Line
+import dev.flutter.plugins.integration_test.FlutterTestRunner;  //Changed by Perfecto
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(FlutterTestRunner.class)
+public class MainActivityTest {
+  @Rule
+  public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
+}
